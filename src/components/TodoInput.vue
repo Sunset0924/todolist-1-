348470bb89137e0f5c8b308c18612c1a957a143c
Yelam <kyl3476@gmail.com>
2022-07-11 21:09:49 +0900

    투두헤더, 인풋 추가

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <input type="text" placeholder="오늘 할 일을 입력하세요.">
+        <button id="inputbutton">입력</button>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss">
+    input{
+        background-color:#fff9f9;
+        width:200px;
+        height:40px;
+        
+    }
+    button{
+        height:40px;
+        border:none;
+        background-color:#ffcece;
+    }
+</style>
