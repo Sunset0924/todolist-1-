25dd443bdc97292f9489fd3d6013f4c18bc308ee
Yelam <kyl3476@gmail.com>
2022-07-26 23:16:02 +0900

    todoitem 추가

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,10 +1,18 @@
 <template>
     <div>
-        {{}}
+        {{number}}. {{whattodo}}
     </div>
 </template>
 
 <script>
+export default{
+    data(){
+        return{
+        number: 1,
+        whattodo:'숙제하기'
+        }
+    }
+}
 </script>
 
 <style lang="scss">
